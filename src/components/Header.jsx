158b6932a9cac88477a1b500590048314d477684
jessasesh <jessica.dison@yahoo.com>
2025-01-14 15:26:24 -0600

    Created Header component that includes Greeting and SocialLinks components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Greeting from "./Greeting";
+import SocialLinks from "./SocialLinks";
+
+function Header() {
+    return (
+        <nav>
+            <Greeting />
+            <SocialLinks />
+        </nav>
+    );
+};
+
+export default Header;
