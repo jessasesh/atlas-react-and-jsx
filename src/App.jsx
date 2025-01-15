c02989e3669d3883d5405f74da08fc2b93e3aed1
jessasesh <jessica.dison@yahoo.com>
2025-01-15 00:11:52 -0600

    Import Footer component to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Section from "./components/Section";
 import HelpfulResource from "./components/HelpfulResource";
 import AboutMe from "./components/AboutMe";
+import Footer from "./components/Footer";
 
 function App() {
   return ( 
@@ -45,6 +46,8 @@ function App() {
       </Section>
 
       <AboutMe />
+
+      <Footer />
     </div>
   );
 }
