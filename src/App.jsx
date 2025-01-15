65474a9c84a520454aec692912ba937e280e9a1e
jessasesh <jessica.dison@yahoo.com>
2025-01-14 22:24:54 -0600

    Added section with title and paragraph w/ content

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ function App() {
   return ( 
     <div className="app">
      <Header />
+     <Section title="What is react?">
+      <p>
+      React is a JavaScript library that makes building responsive,
+      scalable user interfaces straightforward and efficient.
+      By using a component-based structure,
+      it allows you to break your UI into reusable pieces of code.
+      Its virtual DOM boosts performance by only updating what’s necessary,
+      rather than re-rendering the whole page.
+      </p>
+     </Section>
     </div>
   );
 }
